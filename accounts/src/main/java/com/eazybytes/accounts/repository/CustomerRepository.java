2cfaec20de394d1c84e63961b2a4f79e36bee365
kamleshkpritmani <67123648+kamleshkpritmani@users.noreply.github.com>
2025-10-30 10:41:23 -0500

    First Commit
    First Commit

---
+++ accounts/src/main/java/com/eazybytes/accounts/repository/CustomerRepository.java
@@ -0,0 +1,15 @@
+package com.microservicedemo.accounts.repository;
+
+import com.microservicedemo.accounts.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findByMobileNumber(String mobileNumber);
+
+
+}
---
+++ message/src/test/java/com/eazybytes/message/MessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicedemo.message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
